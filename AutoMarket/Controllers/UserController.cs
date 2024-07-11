cb1b1384d3950ca7eeadd7132c14a7e6665a2233
Daniel <kostoskidaniel13@gmail.com>
2024-07-12 01:41:29 +0200

    Added User CRUD operations

--- AutoMarket/Controllers/UserController.cs
+++ AutoMarket/Controllers/UserController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using AutoMarket.Models;
+using PagedList;
 
 namespace AutoMarket.Controllers
 {
@@ -14,6 +15,7 @@ namespace AutoMarket.Controllers
     {
         private ApplicationDbContext db = new ApplicationDbContext();
         // GET: User
+        [Authorize(Roles = "Admin,Moderator")]
         public ActionResult Index()
         {
             
@@ -21,30 +23,35 @@ namespace AutoMarket.Controllers
         }
 
         // GET: User/Details/5
-        public ActionResult Details(string id)
+        [AllowAnonymous]
+        public ActionResult Details(string id, int? page)
         {
+            int pageNumber = (page ?? 1);
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             ApplicationUser applicationUser = db.Users.Find(id);
+
             if (applicationUser == null)
             {
                 return HttpNotFound();
             }
+            ViewBag.PageNumber = pageNumber;
+            ViewBag.Listings = db.Listings.Where(l => l.User.Email.Equals(applicationUser.Email)).OrderBy(l => l.Created).ToPagedList(pageNumber, 4);
             return View(applicationUser);
         }
 
         // GET: User/Create
         public ActionResult Create()
         {
-            return View();
+            return RedirectToAction("Register", "Account");
         }
 
         // POST: User/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
+        /*[HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "Id,PhoneNumber,City,Name,Email,EmailConfirmed,PasswordHash,SecurityStamp,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEndDateUtc,LockoutEnabled,AccessFailedCount,UserName")] ApplicationUser applicationUser)
         {
@@ -56,9 +63,10 @@ namespace AutoMarket.Controllers
             }
 
             return View(applicationUser);
-        }
+        }*/
 
         // GET: User/Edit/5
+        [Authorize(Roles = "Admin")]
         public ActionResult Edit(string id)
         {
             if (id == null)
@@ -76,6 +84,7 @@ namespace AutoMarket.Controllers
         // POST: User/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize(Roles = "Admin")]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "Id,PhoneNumber,City,Name,Email,EmailConfirmed,PasswordHash,SecurityStamp,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEndDateUtc,LockoutEnabled,AccessFailedCount,UserName")] ApplicationUser applicationUser)
@@ -90,6 +99,7 @@ namespace AutoMarket.Controllers
         }
 
         // GET: User/Delete/5
+        /*[Authorize(Roles = "Admin")]
         public ActionResult Delete(string id)
         {
             if (id == null)
@@ -102,14 +112,28 @@ namespace AutoMarket.Controllers
                 return HttpNotFound();
             }
             return View(applicationUser);
-        }
+        }*/
 
         // POST: User/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(string id)
+   
+        public ActionResult Delete(string id)
         {
             ApplicationUser applicationUser = db.Users.Find(id);
+            var listings = db.Listings.Where(l => l.User.Email.Equals(applicationUser.Email)).ToList();
+            foreach(var item in listings)
+            {
+                var images = db.Images.Where(i => i.Listing_ID == item.ID).ToList();
+                foreach (var image in images)
+                {
+                    string filePath = image.Path;
+                    if (System.IO.File.Exists(filePath))
+                    {
+                        System.IO.File.Delete(filePath);
+                    }
+                    db.Images.Remove(image);
+                }
+                db.Listings.Remove(item);
+            }
             db.Users.Remove(applicationUser);
             db.SaveChanges();
             return RedirectToAction("Index");
